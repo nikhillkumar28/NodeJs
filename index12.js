a0aa57e6b078b04e2c2d90166da9b4b5003f8ae2
nikhillkumar28 <nikhilkumarnik28@gmail.com>
2026-02-23 11:22:02 +0530

    NodeJs

--- index12.js
+++ index12.js
@@ -1,15 +1,8 @@
-
-import express from 'express';
-import userRouter from './routes/userRouter.js';
-import productRouter from './routes/productRouter.js';
-
-const app = express();
-app.use(express.json());
-
-app.use("/api/users", userRouter);
-app.use("/api/products", productRouter);
-
-const PORT = 8080;
-app.listen(PORT, () => {
-    console.log(`Server Started on port ${PORT}`);
-});
+// Working with Router
+import express from "express"
+import userRouter from "./routes/userRoutes.js"
+import productRouter from "./routes/productRoutes.js"
+const app = express()
+app.listen(8080,()=>console.log("Server started"))
+app.use("/api/users",userRouter)
+app.use("/api/products",productRouter)
