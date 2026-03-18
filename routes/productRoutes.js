3217e43654edea413461c1377aa1f3fec8dd93b4
Nikhil Kumar <149395889+nikhillkumar28@users.noreply.github.com>
2026-03-18 20:30:07 +0530

    NodeJs

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { addProduct,getProducts } from "../controllers/productController.js"
 const productRouter = express.Router()
+
 productRouter.get("/",getProducts)
 productRouter.post("/",addProduct)
 export default productRouter
